6aac58b6cf2740b5d6588e23cc1b62ee8723e5ff
esteban <luigirp64@gmail.com>
2021-01-16 21:15:03 -0600

    Add logo in Login [TL-13]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import { Box, Container, TextField, Typography } from "@material-ui/core";
+import { Box, Container, TextField, Typography, Icon } from "@material-ui/core";
+import LogInLogo from "./LogInLogo";
 
 const IndexPage = () => (
   <>
@@ -16,10 +17,28 @@ const IndexPage = () => (
           variant="h1"
           component="h1"
           gutterBottom
-          style={{ width: "263px" }}
+          style={{
+            width: "263px",
+            height: "336px",
+            left: "24px",
+            top: "30px",
+          }}
         >
           Your To Do List
         </Typography>
+
+        <Icon
+          style={{
+            top: "273px",
+            left: "178px",
+            width: "285px",
+            height: "253px",
+            position: "absolute",
+          }}
+        >
+          <LogInLogo />
+        </Icon>
+
         <TextField
           id="filled-basic"
           label="Email"
@@ -27,6 +46,13 @@ const IndexPage = () => (
           type="email"
           variant="filled"
           fullWidth
+          style={{
+            position: "absolute",
+            left: "6.4%",
+            right: "6.13%",
+            top: "86.96%",
+            bottom: "4.65%",
+          }}
         />
       </Box>
     </Container>

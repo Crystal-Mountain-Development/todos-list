253cc950bf96fb4abe5a08eb196597426b29d326
Esteban <Luigirp64@gmail.com>
2021-02-06 15:35:44 -0600

    Code Review [TL-16]

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -6,6 +6,8 @@ import {
   TextField,
   Typography,
   Button,
+  createStyles,
+  makeStyles,
 } from "@material-ui/core";
 import { gql, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
@@ -19,30 +21,31 @@ const SEND_SIGN_TOKEN = gql`
   }
 `;
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    form: {
+      height: "100%",
+    },
+  })
+);
+
+const IS_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 const IndexPage = () => {
+  const classes = useStyles();
   const router = useRouter();
-  const [userEmail, setMail] = useState("");
-  const [userName, setName] = useState("");
-  const [errorMail, isErrorMail] = useState(false);
-  const [errorName, isErrorName] = useState(false);
-  const [messageErrorMail, setMessageErrorMail]= useState("");
-  const [messageErrorName, setMessageErrorName]= useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [messageErrorMail, setMessageErrorMail] = useState("");
+  const [messageErrorName, setMessageErrorName] = useState("");
 
   const ableToSignIn = useMemo(() => {
-    return (
-      userEmail &&
-      userName &&
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-        userEmail
-      )
-    );
+    return userEmail && userName && IS_EMAIL_REGEX.test(userEmail);
   }, [userEmail, userName]);
 
   const [addUser] = useMutation(SEND_SIGN_TOKEN);
 
-  const onClick = useCallback(() => {
-    if (!userEmail || !userName) return;
-
+  const onSignIn = useCallback(() => {
     (async function () {
       try {
         await addUser({
@@ -51,31 +54,31 @@ const IndexPage = () => {
         router.push("/auth");
       } catch (e) {
         if (e.message == "Already have a account") {
-          setMessageErrorName("")
-          isErrorName(false);
-          isErrorMail(true);
-          setMessageErrorMail(e.message)
+          setMessageErrorName("");
+          setMessageErrorMail(e.message);
         } else {
-          setMessageErrorMail("")
-          isErrorMail(false);
-          isErrorName(true);
-          setMessageErrorName(e.message)
+          setMessageErrorMail("");
+          setMessageErrorName(e.message);
         }
       }
     })();
   }, [userEmail, userName]);
 
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
-    if (!userEmail || !userName) return;
-  }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!ableToSignIn) return;
+      onSignIn();
+    },
+    [ableToSignIn, onSignIn]
+  );
 
   const onChangeEmail = useCallback((e) => {
-    setMail(e.target.value);
+    setUserEmail(e.target.value);
   }, []);
 
-  const onChangeName = useCallback((e) => {errorName
-    setName(e.target.value);
+  const onChangeName = useCallback((e) => {
+    setUserName(e.target.value);
   }, []);
 
   return (
@@ -88,113 +91,70 @@ const IndexPage = () => {
       </Head>
       <Container maxWidth="xs">
         <Box
-          display="flex"
           position="relative"
+          display="flex"
           flexDirection="column"
-          justifyContent="space-between"
           height="100vh"
           py={3}
         >
-          <div
-            style={{
-              position: "absolute",
-              left: "4.27%",
-              right: "4.27%",
-              top: "2.4%",
-              bottom: "87.41%",
-            }}
-          >
-            <Button
-              onClick={() => router.push("/")}
-              style={{
-                position: "absolute",
-                left: "7.29%",
-                right: "82.22%",
-                top: "0%",
-                bottom: "64.71%",
-                display: "flex",
-                alignItems: "center",
-                width: "21%",
-              }}
-            >
-              {"< Back"}
-            </Button>
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{
-                position: "absolute",
-                left: "0%",
-                right: "11.37%",
-                top: "47.06%",
-                bottom: "0%",
-                width: "100%",
-              }}
-            >
+          <Box>
+            <Button onClick={() => router.push("/")}>{"< Back"}</Button>
+            <Typography variant="h4" component="h4">
               Creating an account
             </Typography>
-          </div>
-          <form onSubmit={onSubmit}>
-            <TextField
-             helperText={messageErrorMail}
-              error={errorMail}
-              value={userEmail}
-              onChange={onChangeEmail}
-              id="filled-basic"
-              label="Email"
-              placeholder="example@email.com"
-              type="email"
-              variant="filled"
-            
-              style={{
-                width: 327,
-                position: "absolute",
-                left: "6.4%",
-                right: "6.4%",
-                top: "15.89%",
-                bottom: "73.31%",
-              }}
-            />
+          </Box>
+          <form className={classes.form} onSubmit={onSubmit}>
+            <Box
+              height="100%"
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box>
+                <TextField
+                  helperText={messageErrorMail}
+                  error={Boolean(messageErrorMail)}
+                  value={userEmail}
+                  onChange={onChangeEmail}
+                  id="user-email"
+                  name="user-email"
+                  label="Email"
+                  placeholder="example@email.com"
+                  type="email"
+                  variant="filled"
+                  fullWidth
+                  margin="normal"
+                />
 
-            <TextField
-            helperText={messageErrorName}
-              error={errorName}
-              value={userName}
-              onChange={onChangeName}
-              id="filled-basic"
-              label="User name"
-              placeholder="example69"
-              type="text"
-              variant="filled"
-             
-              style={{
-                width: 327,
-                position: "absolute",
-                left: "6.4%",
-                right: "6.4%",
-                top: "30.13%",
-                bottom: "59.07%",
-              }}
-            />
+                <TextField
+                  helperText={messageErrorName}
+                  error={Boolean(messageErrorName)}
+                  value={userName}
+                  onChange={onChangeName}
+                  id="user-name"
+                  name="user-name"
+                  label="User name"
+                  placeholder="Your Name"
+                  type="text"
+                  variant="filled"
+                  margin="normal"
+                  fullWidth
+                />
+              </Box>
+              <Button
+                disabled={!ableToSignIn}
+                onSubmit={onSubmit}
+                onClick={onSignIn}
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+              >
+                SIGN IN
+              </Button>
+            </Box>
           </form>
-          <Button
-            disabled={!ableToSignIn}
-            onSubmit={onSubmit}
-            onClick={onClick}
-            style={{
-              position: "absolute",
-              left: "6.67%",
-              right: "6.4%",
-              top: "87.56%",
-              bottom: "4.65%",
-              width: 326,
-              height: 52,
-              color: "black",
-              backgroundColor: "#bb86fc",
-            }}
-          >
-            SIGN IN
-          </Button>
         </Box>
       </Container>
     </>
